dfc7c316efe533777bab6260d8f98a1186956595
Abhiti Darbar <darbarabhiti@gmail.com>
2020-07-26 05:46:51 +0530

    main.dart: add more features
    Signed-off-by: Abhiti Darbar <darbarabhiti@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -32,16 +32,16 @@ class _MyHomePageState extends State<MyHomePage> {
     int count;
     double grid1, grid2;
     final List<Data> chartData = [
-            Data(2010, 35),
-            Data(2011, 10),
-            Data(2012, 34),
-            Data(2013, 32),
-            Data(2014, 50),
-            Data(2015, 70),
-            Data(2016, 30),
-            Data(2017, 100),
-            Data(2018, 30),
-            Data(2019, 29),
+            Data("2018-01-13 01:00", 35),
+            Data("2018-01-14 01:00", 10),
+            Data("2018-01-15 01:00", 34),
+            Data("2018-01-16 01:00", 32),
+            Data("2018-01-17 01:00", 50),
+            Data("2018-01-18 01:00", 70),
+            Data("2018-01-19 01:00", 30),
+            Data("2018-01-20 01:00", 100),
+            Data("2018-01-21 01:00", 30),
+            Data("2018-01-22 01:00", 29),
         ];
     if (MediaQuery.of(context).orientation == Orientation.landscape) {
       count = 2;
@@ -75,7 +75,9 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      body: SingleChildScrollView(
+      body: 
+            
+      SingleChildScrollView(
         child: Column(
           children: <Widget>[
             Container(
@@ -97,18 +99,45 @@ class _MyHomePageState extends State<MyHomePage> {
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10.0),
                       ),
-                      color: Colors.white,
+                      color: Colors.white,  
                       child: Container(
                         padding: EdgeInsets.fromLTRB(30.0,20.0,50.0,30.0),
                         child: SfCartesianChart(
-                          title: ChartTitle(text: 'Real-Time Log Graph'),
+                          title: ChartTitle(text: 'Real-Time Log Graph',backgroundColor: Colors.blueGrey[100]),
+                          enableAxisAnimation: true,
                           crosshairBehavior: CrosshairBehavior(enable:true),
                           tooltipBehavior: TooltipBehavior(enable: true),
+                          zoomPanBehavior: ZoomPanBehavior(
+                            enableDoubleTapZooming: true,
+                          ),
+                          borderColor: Colors.black,
+                          borderWidth: 2,
+                          primaryXAxis: CategoryAxis(
+                            edgeLabelPlacement: EdgeLabelPlacement.shift,
+                            majorTickLines: MajorTickLines(
+                              color: Colors.black
+                            ),
+                          ),
+                          primaryYAxis: NumericAxis(
+                            majorTickLines: MajorTickLines(
+                               color: Colors.black
+                             ),
+                            title: AxisTitle(
+                                text: 'Number of HTTP requests',
+                                textStyle: TextStyle (
+                                  fontWeight: FontWeight.w700
+                                )
+                            )
+                          ),
                           series: <ChartSeries>[
-                            FastLineSeries<Data, double>(
+                            FastLineSeries<Data,String>(
                                 dataSource: chartData,
                                 xValueMapper: (Data sales, _) => sales.date,
                                 yValueMapper: (Data sales, _) => sales.reqNo,
+                                animationDuration: 1000,
+                                markerSettings: MarkerSettings(
+                                    isVisible: true
+                                )
                             )
                           ]
                         )
@@ -172,6 +201,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class Data {
   Data(this.date, this.reqNo);
-    final double date;
+    final String date;
     final double reqNo;
 }
